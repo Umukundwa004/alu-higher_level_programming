1998ca77e38b513dc14fbeb30aa9b9be2a606dee
Umukundwa004 <v.umukundwa@alustudent.com>
2024-10-21 08:40:42 -0700

    a function that prints a string in uppercase followed by a new line.

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for char in str:
-        if ord(char) >= 97 and ord(char) <= 122:
-            char = chr(ord(char) - 32)
-        print("{}".format(char), end ="")
-    print("")
+
+
+def uppercase(str: str) -> None:
+    for letter in str:
+    
+        if "a" <= letter <= "z":
+            letter = chr(ord(letter) - (ord("a") - ord("A")))
+        print("{}".format(letter), end="")
+    print()
