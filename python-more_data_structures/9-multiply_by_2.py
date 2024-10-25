bafaad2a23ebfc7412c7347b7a68b0acf636cfb9
Umukundwa004 <v.umukundwa@alustudent.com>
2024-10-25 04:06:16 -0700

    change

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    return ([list(map(lambda x: x * x, row)) for row in matrix])
+
+
+def multiply_by_2(a_dictionary: dict) -> dict:
+    """
+    Returns a new dictionary with all values multiplied by 2
+
+    Args:
+        a_dictionary (dict): the dictionary to update (modified in-place)
+
+    Returns:
+        dict: a new dictionary with all values multiplied by 2
+    """
+    return {
+        key: value * 2
+        for key, value in a_dictionary.items()
+        if isinstance(value, int)
+    }
