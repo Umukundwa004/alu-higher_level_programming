d94a563238bf2dfe3a4790b55f753d26f579caf7
Umukundwa004 <v.umukundwa@alustudent.com>
2024-10-25 04:04:33 -0700

    changes

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    if my_list == "":
-        return None
-    biggest = my_list[0]
-    for x in range(1, len(my_list)):
-        if my_list[x] > biggest:
-            biggest = my_list[x]
-    return biggest
+def square_matrix_simple(matrix=[]):
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
