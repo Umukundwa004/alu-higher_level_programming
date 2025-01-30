577d54d87c0065399275898ce9dfc147b96ea70d
Umukundwa004 <v.umukundwa@alustudent.com>
2025-01-30 11:24:34 -0800

    java

---
+++ javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const arg = parseInt(process.argv[2], 10);
+
+if (isNaN(arg)) {
+  console.log(1);
+} else {
+  console.log(factorial(arg));
+}
